8a2209ee5dee71977d8199a3d973e0139645e7c0
tihaia <145658291+tihaia@users.noreply.github.com>
2025-02-20 21:16:28 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Привет, мир!";
+
+echo "Hello, World with echo!";
+print "Hello, World with print!";
+
+$days = 288;
+$message = "Все возвращаются на работу!";
+
+echo $days . " " . $message . "<br>";
+echo "День {$days}, {$message} <br>";
+
+?>
