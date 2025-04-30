6b8998a5e71c4f08c519462762d213f6ad48e16a
tihaia <145658291+tihaia@users.noreply.github.com>
2025-05-01 00:31:18 +0300

    Update db.php

--- lab5/todo-app/src/db.php
+++ lab5/todo-app/src/db.php
@@ -1,14 +1,16 @@
 <?php
 
 /**
- * Возвращает подключение к базе данных
+ * Получает подключение к базе данных.
  *
  * @return PDO
  */
 function getPDO(): PDO
 {
+    /** @var array $config Конфигурация подключения */
     $config = require __DIR__ . '/../config/db.php';
 
+    /** @var string $dsn DSN-строка для подключения */
     $dsn = "mysql:host={$config['host']};dbname={$config['dbname']};charset=utf8mb4";
 
     try {
